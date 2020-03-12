ac44f77eeebaf798fed1dc202fafdee79490fd8b
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-03-12 15:07:57 -0600

    created methods for adding and removing items from player inventory

--- src/player.py
+++ src/player.py
@@ -6,6 +6,7 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def travel(self, direction):
         if getattr(self.current_room, f"{direction}_to"):
@@ -13,3 +14,15 @@ class Player:
             print(self.current_room)
         else:
             print("You cannot move in that direction")
+
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
+
+    def remove_from_inventory(self, item):
+        # search if the item is in the inventory and if so, at what index
+        try:
+            self.inventory.index(item)
+        except:
+            print(f"You don't have {item}")
+            return None
+        # if the item is in the inventory, remove it
